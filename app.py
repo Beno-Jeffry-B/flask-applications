f98ed5005b735b3943cd56489b70478163f30f05
Beno-Jeffry-B <benojeffry08@gmail.com>
2025-05-23 00:39:14 +0530

    First appilication is done..

---
+++ app.py
@@ -0,0 +1,27 @@
+'''
+    --> Hello world API
+    running flask application
+    
+    method 1: 
+    If your file name is other than app.py
+    >> $env:FLASK_APP = "main.py"
+    >> flask run 
+
+    
+    Alternate One-Liner for method 1 
+    >> flask --app main.py run
+
+    method 2: (direct flask run if file name is app.py)  
+    >> flask run 
+
+    Remove-Item Env:FLASK_APP   (make sure to do ...if u run prg using first method and trying out direct flask run
+                                wont run so... remove $env first and run)
+
+'''
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/") #is called as decorators
+def hello_world():
+    return "hello_world!"
