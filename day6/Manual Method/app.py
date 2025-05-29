70bd086b4265df8509204da400d970e332b8f361
Beno-Jeffry-B <benojeffry08@gmail.com>
2025-05-29 19:18:22 +0530

    db created

--- day6/Manual Method/app.py
+++ day6/Manual Method/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 #database req
-app.config['SQLALCHEMEY_DATABASE_URI'] = "sqlite:///ItemDB.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///ItemDB.db"
 db  = SQLAlchemy(app)
 
 
@@ -11,12 +11,26 @@ db  = SQLAlchemy(app)
 
 #define the model
 class Items(db.Model):
-    id = db.Column(db.Integer,primarykey=True)
+    id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(30))
     price = db.Column(db.Float)
     
+
+def create_db():
+    
+    with app.app_context():
+        db.create_all()
+
+    print("db created ")
+
+
 
 
 @app.route("/")
 def sample_form():
     return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+    create_db()
