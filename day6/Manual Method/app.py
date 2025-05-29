0a4126f550b00a41952d1e71923611e58dd97032
Beno-Jeffry-B <benojeffry08@gmail.com>
2025-05-29 19:52:59 +0530

    values are stored in DB successfully..!

--- day6/Manual Method/app.py
+++ day6/Manual Method/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,url_for,redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -26,11 +26,29 @@ def create_db():
 
 
 
-@app.route("/")
+@app.route("/",methods = ['GET','POST'])   # By default, Flask only allows GET requests. or else "405 Method Not Allowed" wil occur
 def sample_form():
-    return render_template("index.html")
+    if request.method == "POST":
+        name = request.form["input_name"]
+        price = request.form["input_price"]
 
+        item1 = Items(name=name,price=price)
+        db.session.add(item1)
+        db.session.commit()
+
+        print("added to the Db successfully")
+
+        return redirect(url_for('sample_form')) # redirect to GET after POST
+
+    else:                                       
+        return render_template("index.html")     # for GET or after redirect, just render form
 
 
 if __name__ == "__main__":
     create_db()
+
+
+
+#Scenario	    What to do
+#GET request	Return form template (blank form)
+#POST request   Process data,  redirect to GET route  (recommended)
