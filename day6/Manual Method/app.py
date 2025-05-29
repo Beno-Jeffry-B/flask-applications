ab00364d0d896cf41a9e07b6b9b7b31bc2ce03a7
Beno-Jeffry-B <benojeffry08@gmail.com>
2025-05-29 19:11:19 +0530

    defined DBmodel

--- day6/Manual Method/app.py
+++ day6/Manual Method/app.py
@@ -1,7 +1,20 @@
 from flask import Flask,render_template
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+#database req
+app.config['SQLALCHEMEY_DATABASE_URI'] = "sqlite:///ItemDB.db"
+db  = SQLAlchemy(app)
+
+
+
+
+#define the model
+class Items(db.Model):
+    id = db.Column(db.Integer,primarykey=True)
+    name = db.Column(db.String(30))
+    price = db.Column(db.Float)
+    
 
 
 @app.route("/")
